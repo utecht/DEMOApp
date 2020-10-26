dcf66dbdd4f2a946d90880bdd91b4086bb9a1612
Joseph Utecht <jrutecht@uams.edu>
2020-10-25 19:50:17 -0500

    added filters to providers and locations

--- screens/LocationsScreen.js
+++ screens/LocationsScreen.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   FlatList,
+  StyleSheet,
+  TextInput,
 } from 'react-native';
 
 import LocationPreview from '../components/LocationPreview';
 import LOCATIONS from '../orbweaver/locations.json';
 
 const LocationsScreen = ({ navigation }) => {
+  const [text, setText] = useState('');
+
   function navTo(location){
     navigation.navigate('Location Detail', {
         location:location
@@ -16,13 +20,37 @@ const LocationsScreen = ({ navigation }) => {
   const renderItem = ({item, index, separators}) =>
       <LocationPreview location={item} navigateTo={navTo}/>;
 
+  const locations = LOCATIONS.filter(row =>
+    text.length === 0 ||
+    row.name.toLowerCase().indexOf(text.toLowerCase()) >= 0
+  );
+
+  const styles = StyleSheet.create({
+    filterBox: {
+      height: 30,
+      width: '100%',
+      backgroundColor: 'white',
+      borderRadius: 25,
+      margin: 5,
+      paddingLeft: 5,
+    }
+  });
+
   return (
-    <FlatList
-      style={{height: '100%'}}
-      data={LOCATIONS}
-      renderItem={renderItem}
-      keyExtractor={(item)=>item.url}>
-    </FlatList>
+    <>
+      <TextInput
+        style={styles.filterBox}
+        placeholder="Search for Locations"
+        onChangeText={text => setText(text)}
+        defaultValue={text}
+      />
+      <FlatList
+        style={{height: '100%'}}
+        data={locations}
+        renderItem={renderItem}
+        keyExtractor={(item)=>item.url}>
+      </FlatList>
+    </>
   )
 }
 
