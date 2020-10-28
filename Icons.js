de02826c9736ed2284e5c8a876ad7c51b8602ea2
Joseph Utecht <jrutecht@uams.edu>
2020-10-27 20:48:18 -0500

    added icons for areas of care

--- Icons.js
+++ Icons.js
@@ -9,5 +9,19 @@ Icons['provider'] = require('./images/icons/Provider').default;
 Icons['bill'] = require('./images/icons/Bill').default;
 Icons['calendar'] = require('./images/icons/Calendar').default;
 Icons['doctor_bag'] = require('./images/icons/DoctorBag').default;
+Icons['ambulance'] = require('./images/icons/Ambulance').default;
+Icons['ear'] = require('./images/icons/Ear').default;
+Icons['eye'] = require('./images/icons/Eye').default;
+Icons['digital_health'] = require('./images/icons/Health').default;
+Icons['mental_health'] = require('./images/icons/MentalHealth').default;
+Icons['stomach'] = require('./images/icons/Stomach').default;
+Icons['diabetes'] = require('./images/icons/SugarBloodLevel').default;
+Icons['tomography'] = require('./images/icons/Tomography').default;
+Icons['tooth'] = require('./images/icons/Tooth').default;
+Icons['brain'] = require('./images/icons/Brain').default;
+Icons['hearing'] = require('./images/icons/Hearing').default;
+Icons['kidney'] = require('./images/icons/Kidney').default;
+Icons['heart'] = require('./images/icons/Heart').default;
+Icons['neurosurgery'] = require('./images/icons/Neurosurgery').default;
 
 export default Icons
--- screens/AreasOfExpertiseScreen.js
+++ screens/AreasOfExpertiseScreen.js
@@ -13,18 +13,181 @@ import {
   Text,
   TouchableHighlight,
 } from 'react-native';
-
-import aoes from '../orbweaver/expertise.json';
+import IconButton from '../components/IconButton';
+import { UAMSRed } from '../UAMSColors';
 
 const AreasOfExpertiseScreen = ({ navigation }) => {
+
+  const styles = StyleSheet.create({
+    container: {
+      width: '100%',
+      backgroundColor: UAMSRed,
+      display: 'flex',
+      flexDirection: 'row',
+      flexWrap: 'wrap'
+    },
+    button: {
+      width: 100,
+      height: 100
+    }
+  });
+
   return (
-    <View>
-      <Text>Our Areas of Expertise</Text>
-      {Object.entries(aoes).map((arr)=>{
-        let key = arr[0];
-        let value = arr[1];
-        return(<Text key={key}>{value.name}</Text>)
-      })}
+    <View style={styles.container}>
+      <View style={styles.button}>
+        <IconButton
+          label="Psychiatry"
+          iconColor="red"
+          size={50}
+          icon="mental_health"
+          onPress={() =>
+            navigation.navigate("Providers")
+          }
+        />
+      </View>
+      <View style={styles.button}>
+        <IconButton
+          label="Burn Care"
+          iconColor="red"
+          size={50}
+          icon="ambulance"
+          onPress={() =>
+            navigation.navigate("Providers")
+          }
+        />
+      </View>
+      <View style={styles.button}>
+        <IconButton
+          label="Cancer Care"
+          iconColor="red"
+          size={50}
+          icon="tomography"
+          onPress={() =>
+            navigation.navigate("Providers")
+          }
+        />
+      </View>
+      <View style={styles.button}>
+        <IconButton
+          label="Dentile Care"
+          iconColor="red"
+          size={50}
+          icon="tooth"
+          onPress={() =>
+            navigation.navigate("Providers")
+          }
+        />
+      </View>
+      <View style={styles.button}>
+        <IconButton
+          label="Diabetes and Endocrinology Care"
+          iconColor="red"
+          size={50}
+          icon="diabetes"
+          onPress={() =>
+            navigation.navigate("Providers")
+          }
+        />
+      </View>
+      <View style={styles.button}>
+        <IconButton
+          label="Digestive Health"
+          iconColor="red"
+          size={50}
+          icon="stomach"
+          onPress={() =>
+            navigation.navigate("Providers")
+          }
+        />
+      </View>
+      <View style={styles.button}>
+        <IconButton
+          label="Ear Nose and Throat Care"
+          iconColor="red"
+          size={50}
+          icon="ear"
+          onPress={() =>
+            navigation.navigate("Providers")
+          }
+        />
+      </View>
+      <View style={styles.button}>
+        <IconButton
+          label="Emergency Care"
+          iconColor="red"
+          size={50}
+          icon="ambulance"
+          onPress={() =>
+            navigation.navigate("Providers")
+          }
+        />
+      </View>
+      <View style={styles.button}>
+        <IconButton
+          label="Eye Care"
+          iconColor="red"
+          size={50}
+          icon="eye"
+          onPress={() =>
+            navigation.navigate("Providers")
+          }
+        />
+      </View>
+      <View style={styles.button}>
+        <IconButton
+          label="Hearing and Balance"
+          iconColor="red"
+          size={50}
+          icon="hearing"
+          onPress={() =>
+            navigation.navigate("Providers")
+          }
+        />
+      </View>
+      <View style={styles.button}>
+        <IconButton
+          label="Heart and Vascular Care"
+          iconColor="red"
+          size={50}
+          icon="heart"
+          onPress={() =>
+            navigation.navigate("Providers")
+          }
+        />
+      </View>
+      <View style={styles.button}>
+        <IconButton
+          label="Neurology"
+          iconColor="red"
+          size={50}
+          icon="brain"
+          onPress={() =>
+            navigation.navigate("Providers")
+          }
+        />
+      </View>
+      <View style={styles.button}>
+        <IconButton
+          label="Neurosurgery"
+          iconColor="red"
+          size={50}
+          icon="neurosurgery"
+          onPress={() =>
+            navigation.navigate("Providers")
+          }
+        />
+      </View>
+      <View style={styles.button}>
+        <IconButton
+          label="Organ Transplant"
+          iconColor="red"
+          size={50}
+          icon="kidney"
+          onPress={() =>
+            navigation.navigate("Providers")
+          }
+        />
+      </View>
     </View>
     )
 }
